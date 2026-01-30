285a2ed94cfc5ae667753ae67363b0d1a8f2d004
admin <admin@MACHINE>
2026-01-30 16:46:43 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react';
-import axios from 'axios';
 import SearchBar from './components/SearchBar';
 import Pagination from './components/Pagination';
 import searchApi from './searchApi';
